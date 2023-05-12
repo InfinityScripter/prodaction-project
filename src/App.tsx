5026ed1da3217b76f567361e49a9fb4c535a8ae6
kot <talalaev.misha@gmail.com>
2023-05-12 14:20:20 +0400

    add routing The bundle is divided into chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
-import './index.scss';
+import React, { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./index.scss";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
   return (
-    <div className='app' >
-      <Counter/>
-      Hi how are you?
+    <div className="app">
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
