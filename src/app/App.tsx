119c574846186aa99cc6455847ad9fea04a37a7f
kot <talalaev.misha@gmail.com>
2023-05-15 17:38:28 +0400

    add fsd (feature-sliced-design) methodology add absolute import in application

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
 import React, { Suspense, useContext } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import "./styles/index.scss";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import "../styles/index.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { MainPage } from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
-  const bool = true;
   return (
     <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle theme</button>
@@ -16,8 +15,8 @@ const App = () => {
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/"} element={<MainPageAsync />} />
-          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPage />} />
+          <Route path={"/about"} element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
