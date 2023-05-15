af281c62cadbbf349f121ce6a34f7194ead487bb
kot <talalaev.misha@gmail.com>
2023-05-15 18:30:46 +0400

    add routes to shared/config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import "../styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
+import { AppRouter } from "app/providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -13,12 +12,7 @@ const App = () => {
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
