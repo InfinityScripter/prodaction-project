bd4e30dfbaa5d74980f115b06a71ce480b5d8c2b
kot <talalaev.misha@gmail.com>
2023-05-26 14:35:12 +0400

    add sidebar styles add new const for Sidebar.module.scss change theme-switcher in Sidebar.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,16 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "app/providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+      </div>
     </div>
   );
 };
